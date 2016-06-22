b0a40c71392a952ff22a7a98ed173e0054902d88
David Livingstone <david@david.tl>
2016-06-22 13:29:26 -0700

    finished code challenge

---
+++ class-2/code-challenge.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+func oddBall(userArray: [Int]) -> [Int] {
+    var result = [Int]()
+    
+    for item in userArray {
+        if item % 2 > 0 {
+            result.append(item)
+        }
+    }
+    return result
+}
+
+let myArray = [1, 2, 5, 7, 12, 24, 67]
+
+oddBall(myArray)
+
+//for name in CIFilter.filterNamesInCategories(nil){
+//    print(name)
+//}
+
