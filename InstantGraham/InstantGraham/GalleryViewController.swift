616bbf5a08cdc9ae2b3c0f78751fc97bfccba3c8
David Livingstone <david@david.tl>
2016-06-24 13:51:36 -0700

    added collection view basics

---
+++ InstantGraham/InstantGraham/GalleryViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GalleryViewController.swift
+//  InstantGraham
+//
+//  Created by David Livingstone on 6/24/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var colletionView: UICollectionView!
+    
+    var posts = [Post]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("", forIndexPath: indexPath)
+        let post = posts[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstantGraham/InstantGraham/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  InstantGraham
+//
+//  Created by David Livingstone on 6/24/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+ 
+    
+    
+}
